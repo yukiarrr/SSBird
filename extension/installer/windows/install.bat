055c571da1a323218dc69be4f80f659af4d93c03
Yuki Araikawa <mail@yukiarrr.com>
2020-09-06 22:28:12 +0900

    Add taskkill

--- extension/installer/windows/install.bat
+++ extension/installer/windows/install.bat
@@ -4,6 +4,7 @@ set install_dir=%LOCALAPPDATA%\SSBird
 
 reg add "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.yukiarrr.ssbird" /ve /t REG_SZ /d "%install_dir%\com.yukiarrr.ssbird.json" /f
 
+taskkill /f /im SSBird.exe
 xcopy /i /y .\host %install_dir%
 
 echo Success!
